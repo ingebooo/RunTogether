ee484ed6e0d87ff342d42f6a932db4d9d0ca8594
Ingeborg Ødegård Oftedal <Ingeborg.Oftedal@evry.com>
2016-07-19 09:49:52 +0200

    removed

--- wearable/src/main/java/com/example/ingebode/wearable/NodeListernerService.java
+++
@@ -1,9 +0,0 @@
-package com.example.ingebode.wearable;
-
-import com.google.android.gms.wearable.WearableListenerService;
-
-/**
- * Created by ingebode on 30/05/16.
- */
-public class NodeListernerService extends WearableListenerService {
-}
